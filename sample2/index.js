9038825ecf22d9ebb314d1b06f0a18eaf1b73c12
ychonkar <ychonkar@aaxiscommerce.com>
2018-03-11 20:56:40 -0700

    dummy array

--- sample2/index.js
+++ sample2/index.js
@@ -1,3 +1,12 @@
+let products=[
+{"id":"p1", name:"Sony Camera", price:533.23},
+{"id":"p2", name:"LG TV", price:533.23},
+{"id":"p3", name:"GoPRO Camera", price:533.23},
+{"id":"p4", name:"Fire TV", price:30},
+{"id":"p5", name:"Chromecast Video", price:25},
+{"id":"p6", name:"PINE64", price:33.23},
+{"id":"p7", name:"SOny Projector", price:933.23}
+];
 /**
  * Routes the request to the table controller to the correct method.
  *
@@ -5,6 +14,7 @@
  * @param {Express.Request} req - the actual request
  */
 function pimProductRouter(context, req) {
+    
     context.log.info("pim router invoked");
     context.log.info(JSON.stringify(context.bindings));
     context.log.info(JSON.stringify(req));
@@ -44,15 +54,32 @@ function pimProductRouter(context, req) {
 }
  
 function getOneItem(req, res, id) {
-    res.status(200).json({ id: id, message: "getOne" });
+    //res.status(200).json({ id: id, message: "getOne" });
+    for(i=0;i<products.length;i++){
+        if(products[i].id == id){
+            res.status(200).json(products[i]); 
+            return;
+        }
+    }
+    res.status(404).json({error:"No product found with id:"+id});
 }
  
 function getAllItems(req, res) {
-    res.status(200).json({ query: req.query, message: "getAll" });
+    //res.status(200).json({ query: req.query, message: "getAll" });
+    res.status(200).json(products);
 }
  
 function insertItem(req, res) {
-    res.status(200).json({ body: req.body, message: "insert"});
+    //res.status(200).json({ body: req.body, message: "insert"});
+    id= req.body.id;
+    for(i=0;i<products.length;i++){
+        if(products[i].id == id){
+            res.status(500).json({eror:"Duplicate Product ID Error"}); 
+            return;
+        }            
+    }
+    products.push(req.body);    
+    res.status(200).json({message:"Product Added"});
 }
  
 function patchItem(req, res, id) {
